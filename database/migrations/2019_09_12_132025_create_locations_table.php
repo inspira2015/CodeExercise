c1e6e2eb8542f7abee04a0cb61e51c6e791175d8
Daniel Gomez <dgomez@nassaucountynh.gov>
2019-09-12 09:39:21 -0400

    Added Migrations for tables (locations/forecasts)

---
+++ database/migrations/2019_09_12_132025_create_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->decimal('latitude', 10, 4);
+            $table->decimal('longitude', 10, 4);
+            $table->dateTime('last_forecast_update');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
---
+++ database/migrations/2019_09_12_132248_create_forecasts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForecastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forecasts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('locations_id');
+            $table->integer('time');
+            $table->decimal('temperature', 8,2);
+            $table->decimal('precipitation_intensity', 10,4);
+            $table->tinyInteger('precipitation_probability');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forecasts');
+    }
+}
